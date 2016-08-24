2706a549fb39cc68bb6c518a26511538840b19cd
tnaughts <tnaughts@gmail.com>
2016-08-24 14:03:47 -0500

    create file

---
+++ cljr/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cljr::Application.config.secret_key_base = '65a6f9040a4ebcb3ca572830a297313805de72db47bd945c55952da1bbc5c5fa89ee9a9cb3609d612e69d778d74c98eb3ca0e2c3141c1efa2971f83a076313fa'
